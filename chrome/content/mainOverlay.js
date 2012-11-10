5f9e1c62974fae491c5882130d6ddfafda288821
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2012-11-10 23:33:16 +0100

    Implemented FireCompass with AMD

---
+++ chrome/content/mainOverlay.js
@@ -0,0 +1,6 @@
+
+Firebug.registerExtension("firecompass", {
+	id: "firecompass@stueckseln.de"
+});
+
+Firebug.registerTracePrefix("firecompass;", "DBG_FIRECOMPASS", true, "chrome://firecompass/skin/style.css");
