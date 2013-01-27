67c642f4acc2e2ca93356c3e9b6412676cd20a93
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2013-01-27 15:28:49 +0100

    Added stub spec for firecompass.js

---
+++ specs/firecompass_spec.js
@@ -0,0 +1,15 @@
+
+define(["chrome/content/firecompass"], function(Firecompass){
+	console.log("parseCompassComment: ", Firecompass.parseCompassComment);
+
+	describe("Test", function(){
+		it("Test", function(){
+			expect(
+				true
+			).toBe(
+				true
+			);
+		});
+	});
+});
+
